a8778f6b9fe8b4b1c9eb0b278bbe52fc58855b01
Gael Huber <gael.scott.huber@gmail.com>
2014-08-17 16:38:50 -0700

    Adding packet declaration.

--- Shared/Include/BBPacket.h
+++ Shared/Include/BBPacket.h
@@ -12,13 +12,20 @@ namespace BlockBlock
 	*/
 
 	/**
+	* The size of the data portion of the packet
+	*/
+	#define DATA_PACKET_SIZE 104
+
+	/**
 	* Packets are used to send and receive data sent over the network.
 	* The basic format of a packet is as follows:
 	*	uint32 		GAME_ID 	- A "unique" integer that allows us to prune out known irrelevant packets
 	*	uint32 		PACKET_ID 	- A unique counter that indicates the approximate "time" a packet was sent
-	*	uchar[112]	data 		- The first byte in this packet is the number of valid bytes in the packet.
-	*								Any subsequent bytes of the remaining 111 bytes are data values
-	* The total size of each packet is 128 bytes, which can store up to 11 additional integer values + 3 loose bytes
+	*   uint32		MESSAGE_TYPE- A message id represents the identifier for this message so it can be properly
+	*								handled by the receiver of the packet (eg. MOVE, ACTION, PAUSE, etc.)
+	*	uchar[104]	DATA 		- The first byte in this packet is the number of valid bytes in the packet.
+	*								Any subsequent bytes of the remaining 103 bytes are data values
+	* The total size of each packet is 128 bytes, which can store up to 10 additional integer values + 3 loose bytes
 	*/
 	class Packet
 	{
@@ -33,10 +40,34 @@ namespace BlockBlock
 		* @param
 		*	uint The packet ID for this packet
 		* @param
+		*	uint The message type for this packet
+		* @param
 		*	uchar The size of the data in this packet
 		* @param
 		*	void* A pointer to the data being transfered to this packet
 		*/
+		Packet(uint, uint, uchar, void*);
+
+		/**
+		* Copy constructor
+		* @param
+		*	const Packet& The packet to copy
+		*/
+		Packet(const Packet&);
+
+		/**
+		* Destructor
+		*/
+		~Packet();
+
+		/**
+		* Assignment operator
+		* @param
+		*	const Packet& The packet to assign
+		* @return
+		*	Packet& The resultant packet
+		*/
+		Packet& operator=(const Packet&);
 
 	private:
 		/**
@@ -50,9 +81,14 @@ namespace BlockBlock
 		uint _packetId;
 
 		/**
+		* The message type for this packet
+		*/
+		uint _messageType;
+
+		/**
 		* The data stored within this packet
 		*/
-		uchar _data[112];
+		uchar _data[DATA_PACKET_SIZE];
 	};
 
 	/** @} */
